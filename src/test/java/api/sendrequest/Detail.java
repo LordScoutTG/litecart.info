0a5044b60c96e5f05ea25ff0163e9b68979dbecc
Lord Scout <129535741+LordScoutTG@users.noreply.github.com>
2023-09-05 16:58:42 +0300

    Delete src/test/java/api directory

--- src/test/java/api/sendrequest/Detail.java
+++
@@ -1,8 +0,0 @@
-package api.sendrequest;
-
-import lombok.Data;
-
-@Data
-public class Detail {
-    private String reason;
-}
--- src/test/java/api/sendrequest/ResponseError.java
+++
@@ -1,8 +0,0 @@
-package api.sendrequest;
-
-import lombok.Data;
-
-@Data
-public class ResponseError {
-    private Detail detail;
-}
