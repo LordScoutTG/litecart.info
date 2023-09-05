04cf53fc6f4e52b50c86fd982c7f8712fad96b3b
Lord Scout <129535741+LordScoutTG@users.noreply.github.com>
2023-09-05 17:00:59 +0300

    Delete src/test/java/pageObject directory

--- src/test/java/pageObject/object/PageBase.java
+++
@@ -1,9 +0,0 @@
-package pageObject.object;
-
-import org.openqa.selenium.WebDriver;
-
-public class PageBase {
-    public boolean hasOpened(WebDriver driver, String title){
-        return driver.getTitle().equals(title);
-    }
-}
